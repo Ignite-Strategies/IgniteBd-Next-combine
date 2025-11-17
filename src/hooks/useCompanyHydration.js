1db4cc5efbe411a6dd3132fbbfc5bf3d32cb1f64
Adam Cole <adam@gofastcrushgoals.com>
2025-11-16 22:06:07 -0500

    Add workPackages to convenience getters in useCompanyHydration

--- src/hooks/useCompanyHydration.js
+++ src/hooks/useCompanyHydration.js
@@ -152,6 +152,7 @@ export function useCompanyHydration(companyHQId) {
     proposals: data.proposals,
     phaseTemplates: data.phaseTemplates,
     deliverableTemplates: data.deliverableTemplates,
+    workPackages: data.workPackages,
     stats: data.stats,
   };
 }
