906c008f660787342b32901a073f594fe9dd2582
Adam Cole <adam@gofastcrushgoals.com>
2025-12-12 07:15:53 -0500

    Add display swap to font config to reduce preload warnings

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,13 @@ import Providers from "./providers.jsx";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
