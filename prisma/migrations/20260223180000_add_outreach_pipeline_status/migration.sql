d8985e25ab0a1c4ca0c202d6de9d37eabe1e0563
Adam Cole <adam@gofastcrushgoals.com>
2026-02-23 22:27:41 -0500

    Revert "Add outreach pipeline status tracking for contacts"
    This reverts commit 1ef229a6bc096cf5d866e842a76bc16acd5bc48c.

--- prisma/migrations/20260223180000_add_outreach_pipeline_status/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "OutreachPipelineStatus" AS ENUM ('NEED_TO_ENGAGE', 'ENGAGED_AWAITING_RESPONSE', 'RESPONDED');
-
--- AlterTable
-ALTER TABLE "contacts" ADD COLUMN "outreachPipelineStatus" "OutreachPipelineStatus" DEFAULT 'NEED_TO_ENGAGE';
-
--- CreateIndex
-CREATE INDEX "contacts_outreachPipelineStatus_idx" ON "contacts"("outreachPipelineStatus");
